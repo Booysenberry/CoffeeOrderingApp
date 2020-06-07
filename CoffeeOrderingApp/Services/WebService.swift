5749d27c9849beefbf4c3e42eb70e92c93d3fc89
Booysenberry <brad.booysen@gmail.com>
2020-06-08 06:56:02 +1200

    Add create order function

--- CoffeeOrderingApp/Services/WebService.swift
+++ CoffeeOrderingApp/Services/WebService.swift
@@ -10,6 +10,38 @@ import Foundation
 
 class WebService {
     
+    func createCoffeeOrder(order: Order, completion: @escaping (CreateOrderResponse?) -> ()) {
+        
+        //Create url
+        guard let url = URL(string: "https://island-bramble.glitch.me/orders") else {
+            fatalError("Invalid URL")
+        }
+        
+        // Setup POST request
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        request.addValue("application/json", forHTTPHeaderField: "Content-type")
+        request.httpBody = try? JSONEncoder().encode(order)
+        
+        URLSession.shared.dataTask(with: request) { data, response, error in
+            
+            guard let data = data, error == nil else {
+                
+                DispatchQueue.main.async {
+                    completion(nil)
+                }
+                return
+            }
+            
+            let createOrderResponse = try? JSONDecoder().decode(CreateOrderResponse.self, from: data)
+            
+            DispatchQueue.main.async {
+                completion(createOrderResponse)
+            }
+        }.resume()
+        
+    }
+    
     func getAllOrders(completion: @escaping ([Order]?) -> ()) {
         
         // create url
